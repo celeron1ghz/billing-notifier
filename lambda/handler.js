c8bba65e1751e07b387825342140a4151146d4dd
celeron1ghz <celeron1ghz@gmail.com>
2017-08-12 23:19:59 +0900

    add lambda main script

---
+++ lambda/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const aws = require('aws-sdk');
+const cb  = new aws.CodeBuild();
+
+module.exports.kicker = (event, context, callback) => {
+    cb.startBuild({ projectName: 'billing-notifier-etc' }, function(err, ret){
+        callback(err, ret.build);
+    })
+};
+
+module.exports.status_getter = (event, context, callback) => {
+    cb.batchGetBuilds({ ids: [event.id] }, function(err, ret){
+        callback(err, ret.builds[0]);
+    });
+};
