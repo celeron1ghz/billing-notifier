40850bddf074dfb25f16fea19a89aaa5f98fd620
celeron1ghz <celeron1ghz@gmail.com>
2017-11-16 21:22:04 +0900

    fixed to work

--- parse_etc_meisai.js
+++ parse_etc_meisai.js
@@ -2,7 +2,7 @@
 
 process.env.AWS_REGION = 'ap-northeast-1';
 
-const nightmare = new require('nightmare')();
+const nightmare = new require('nightmare')({ show: true });
 const vo   = require('vo');
 const cred = require('credstash-promise');
 
@@ -29,28 +29,33 @@ vo(function*(){
             const css     = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
             const rm_nbsp = (val)              => val.replace(/&nbsp;/g, "");
 
-            return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
-                const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
-                const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
-                const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
-                const from  = td1[0].split('<br>');
-                const to    = td1[1].split('<br>');
-                const price = td2.split('<br>');
-                const meta  = td4.split('<br>');
+            try {
+                return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
+                    const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
+                    const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
+                    const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
+                    const from  = td1[0].split('<br>');
+                    const to    = td1[1].split('<br>');
+                    const price = td2.split('<br>');
+                    const meta  = td4.split('<br>');
 
-                return {
-                    from_date:  rm_nbsp(from[0]),
-                    from_time:  rm_nbsp(from[1]),
-                    from_place: rm_nbsp(from[2]),
-                    to_date:    to[0],
-                    to_time:    to[1],
-                    to_place:   to[2],
-                    price:      price[2].replace(/,/g, ""),
-                    type:       meta[0],
-                    car_no:     meta[1],
-                    etc_no:     meta[2],
-                }
-            });
+                    return {
+                        from_date:  rm_nbsp(from[0]),
+                        from_time:  rm_nbsp(from[1]),
+                        from_place: rm_nbsp(from[2]),
+                        to_date:    to[0],
+                        to_time:    to[1],
+                        to_place:   to[2],
+                        price:      price[2].replace(/,/g, ""),
+                        type:       meta[0],
+                        car_no:     meta[1],
+                        etc_no:     meta[2],
+                    }
+                });
+            } catch(e) {
+                console.log("Error on parse:", e);
+                return [];
+            }
         });
 
         meisai.shift();
