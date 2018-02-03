c05ade452b7f5bac2dd9fd75946e072e39cfc5c6
celeron1ghz <celeron1ghz@gmail.com>
2018-02-03 09:01:55 +0900

    refactor

--- parser/etc_meisai.js
+++ parser/etc_meisai.js
@@ -1,85 +1,99 @@
 'use strict';
 
-process.env.AWS_REGION = 'ap-northeast-1';
-
-//const nightmare = new require('nightmare')();
-const nightmare = new require('nightmare')({ show: true });
 const vo  = require('vo');
-const aws = require('aws-sdk');
-const ssm = new aws.SSM();
 
-vo(function*(){
-    const url  = 'https://www2.etc-meisai.jp/etc/R?funccode=1013000000&nextfunc=1013000000';
-    const id   = (yield ssm.getParameter({ Name: '/etc_meisai/user_id',  WithDecryption: true }).promise() ).Parameter.Value;
-    const pass = (yield ssm.getParameter({ Name: '/etc_meisai/password', WithDecryption: true }).promise() ).Parameter.Value;
-
-    let result = [];
-    nightmare.viewport(1000, 1000)
-        .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-        .goto(url)
-        // login
-        .type("input[name=risLoginId]", id)
-        .type("input[name=risPassword]", pass)
-        .click("input[name=focusTarget]")
-        .wait("table.meisaiinfo")
-        // choose previous month
-        //.click("table.meisaiinfo > tbody > tr:nth-child(3) > td > table:nth-child(3) > tbody > tr > td:nth-last-child(3) button")
-        //.wait(1000)
-
-    while (true)   {
-        const meisai = yield nightmare.evaluate(function () {
-            const css     = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
-            const rm_nbsp = (val)              => val.replace(/&nbsp;/g, "");
-
-            try {
-                return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
-                    const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
-                    const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
-                    const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
-                    const from  = td1[0].split('<br>');
-                    const to    = td1[1].split('<br>');
-                    const price = td2.split('<br>');
-                    const meta  = td4.split('<br>');
-
-                    return {
-                        from_date:  rm_nbsp(from[0]),
-                        from_time:  rm_nbsp(from[1]),
-                        from_place: rm_nbsp(from[2]),
-                        to_date:    to[0],
-                        to_time:    to[1],
-                        to_place:   to[2],
-                        price:      price[2].replace(/,/g, ""),
-                        type:       meta[0],
-                        car_no:     meta[1],
-                        etc_no:     meta[2],
-                    }
-                });
-            } catch(e) {
-                console.log("Error on parse:", e);
-                return [];
-            }
-        });
-
-        meisai.shift();
-        result = result.concat(meisai);
-
-        const next_button = yield nightmare.evaluate(
-            () => document.querySelector("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
-        );
-
-        if (!next_button)  {
-            break;
-        }
-
-        nightmare.click("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
-            .wait("table.meisaiinfo")
-            .wait(1000);
+class EtcMeisaiParser {
+    constructor(user_id, password) {
+        this.user_id  = user_id;
+        this.password = password;
     }
 
-    yield nightmare.end();
-    return result;
-})
-.then(data => {
-    console.log(JSON.stringify(data));
-})
-.catch(err => { throw new Error(err) })
+    parse() {
+        //const nightmare = new require('nightmare')();
+        const nightmare = new require('nightmare')({ show: true });
+        const id   = this.user_id;
+        const pass = this.password;
+
+        return vo(function*(){
+            const url  = 'https://www2.etc-meisai.jp/etc/R?funccode=1013000000&nextfunc=1013000000';
+
+            let result = [];
+            nightmare.viewport(1000, 1000)
+                .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+                .goto(url)
+                // login
+                .type("input[name=risLoginId]", id)
+                .type("input[name=risPassword]", pass)
+                .click("input[name=focusTarget]")
+                .wait("table.meisaiinfo")
+                // choose previous month
+                .click("table.meisaiinfo > tbody > tr:nth-child(3) > td > table:nth-child(3) > tbody > tr > td:nth-last-child(3) button")
+                .wait(1000)
+
+            while (true)   {
+                const meisai = yield nightmare.evaluate(function () {
+                    const css     = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
+                    const rm_nbsp = (val)              => val.replace(/&nbsp;/g, "");
+
+                    try {
+                        return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
+                            const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
+                            const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
+                            const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
+                            const from  = td1[0].split('<br>');
+                            const to    = td1[1].split('<br>');
+                            const price = td2.split('<br>');
+                            const meta  = td4.split('<br>');
+
+                            return {
+                                from_date:  rm_nbsp(from[0]),
+                                from_time:  rm_nbsp(from[1]),
+                                from_place: rm_nbsp(from[2]),
+                                to_date:    to[0],
+                                to_time:    to[1],
+                                to_place:   to[2],
+                                price:      price[2].replace(/,/g, ""),
+                                type:       meta[0],
+                                car_no:     meta[1],
+                                etc_no:     meta[2],
+                            }
+                        });
+                    } catch(e) {
+                        console.log("Error on parse:", e);
+                        return [];
+                    }
+                });
+
+                meisai.shift();
+                result = result.concat(meisai);
+
+                const next_button = yield nightmare.evaluate(
+                    () => document.querySelector("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
+                );
+
+                if (!next_button)  {
+                    break;
+                }
+
+                nightmare.click("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
+                    .wait("table.meisaiinfo")
+                    .wait(1000);
+            }
+
+            yield nightmare.end();
+            return result;
+        })
+    }
+}
+
+vo(function*(){
+    process.env.AWS_REGION = 'ap-northeast-1';
+    const aws = require('aws-sdk');
+    const ssm = new aws.SSM();
+    const id   = (yield ssm.getParameter({ Name: '/etc_meisai/user_id',  WithDecryption: true }).promise() ).Parameter.Value;
+    const pass = (yield ssm.getParameter({ Name: '/etc_meisai/password', WithDecryption: true }).promise() ).Parameter.Value;
+    const ret = yield new EtcMeisaiParser(id, pass)
+        .parse()
+        .catch(err => { throw new Error(err) })
+    console.log(JSON.stringify(ret))
+}).catch(err => { throw new Error(err) })
--- parser/viewcard_meisai.js
+++ parser/viewcard_meisai.js
@@ -1,76 +1,90 @@
 'use strict';
 
-process.env.AWS_REGION = 'ap-northeast-1';
-
-//const nightmare = new require('nightmare')({ show: true });
-const nightmare = new require('nightmare')();
 const vo  = require('vo');
-const aws = require('aws-sdk');
-const ssm = new aws.SSM();
 
-vo(function*(){
-    const url  = 'https://viewsnet.jp/';
-    const id   = (yield ssm.getParameter({ Name: '/viewcard/user_id',  WithDecryption: true }).promise() ).Parameter.Value;
-    const pass = (yield ssm.getParameter({ Name: '/viewcard/password', WithDecryption: true }).promise() ).Parameter.Value;
-
-    let result = [];
-    nightmare.viewport(1000, 1000)
-        .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-        .goto(url)
-        // login
-        .type("input[name=id]", id)
-        .type("input[name=pass]", pass)
-        .click("input[type=image]")
-        .wait(1000)
-        // 
-        .click("a#vucGlobalNavi_LnkV0300_001Header")
-        .wait(1000)
-        //
-        .click("a#LnkYotei")
-        .wait(1000)
-
-    while (true)   {
-        const meisai = yield nightmare.evaluate(function () {
-            const css = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
-
-            const trs = css(document, "div#DivDetailInfo table tbody tr");
-            trs.shift();
-
-            return trs.map(function(tr){
-                const td1 = css(tr, "td:nth-child(1)");
-                const td2 = css(tr, "td:nth-child(2)");
-                const td3 = css(tr, "td:nth-child(3)");
-                const td4 = css(tr, "td:nth-child(4)");
-
-                return {
-                    date:    (td1.length != 0 ? css(td1[0], "span").map(e => e.textContent).join("/") : ""),
-                    card_no: (td2.length != 0 ? css(td2[0], "span")[0].innerHTML : ""),
-                    shop:    (td3.length != 0 ? css(td3[0], "strong")[0].textContent : ""),
-                    price:   (td4.length != 0 ? css(td4[0], "strong")[0].textContent.replace(/,/g, "") : ""),
-                };
-            });
-        });
-
-        meisai.shift();
-        result = result.concat(meisai);
-
-        const next_button = yield nightmare.evaluate(
-            () => document.querySelector("#LnkNextBottom")
-        );
-
-        if (!next_button)  {
-            break;
-        }
-
-        nightmare.click("#LnkNextBottom")
-            .wait("div#DivDetailInfo")
-            .wait(1000);
+class ViewCardParser {
+    constructor(user_id, password) {
+        this.user_id  = user_id;
+        this.password = password;
     }
 
-    yield nightmare.end();
-    return result;
-})
-.then(data => {
-    console.log(JSON.stringify(data));
-})
-.catch(err => { throw new Error(err) })
+    parse() {
+        const nightmare = new require('nightmare')({ show: true });
+        //const nightmare = new require('nightmare')();
+        const id   = this.user_id
+        const pass = this.password;
+
+        return vo(function*(){
+            const url  = 'https://viewsnet.jp/';
+
+            let result = [];
+            nightmare.viewport(1000, 1000)
+                .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+                .goto(url)
+                // login
+                .type("input[name=id]", id)
+                .type("input[name=pass]", pass)
+                .click("input[type=image]")
+                .wait(1000)
+                // 
+                .click("a#vucGlobalNavi_LnkV0300_001Header")
+                .wait(1000)
+                //
+                .click("a#LnkYotei")
+                .wait(1000)
+
+            while (true)   {
+                const meisai = yield nightmare.evaluate(function () {
+                    const css = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
+
+                    const trs = css(document, "div#DivDetailInfo table tbody tr");
+                    trs.shift();
+
+                    return trs.map(function(tr){
+                        const td1 = css(tr, "td:nth-child(1)");
+                        const td2 = css(tr, "td:nth-child(2)");
+                        const td3 = css(tr, "td:nth-child(3)");
+                        const td4 = css(tr, "td:nth-child(4)");
+
+                        return {
+                            date:    (td1.length != 0 ? css(td1[0], "span").map(e => e.textContent).join("/") : ""),
+                            card_no: (td2.length != 0 ? css(td2[0], "span")[0].innerHTML : ""),
+                            shop:    (td3.length != 0 ? css(td3[0], "strong")[0].textContent : ""),
+                            price:   (td4.length != 0 ? css(td4[0], "strong")[0].textContent.replace(/,/g, "") : ""),
+                        };
+                    });
+                });
+
+                meisai.shift();
+                result = result.concat(meisai);
+
+                const next_button = yield nightmare.evaluate(
+                    () => document.querySelector("#LnkNextBottom")
+                );
+
+                if (!next_button)  {
+                    break;
+                }
+
+                nightmare.click("#LnkNextBottom")
+                    .wait("div#DivDetailInfo")
+                    .wait(1000);
+            }
+
+            yield nightmare.end();
+            return result;
+        })
+    }
+}
+
+vo(function*(){
+    process.env.AWS_REGION = 'ap-northeast-1';
+    const aws = require('aws-sdk');
+    const ssm = new aws.SSM();
+    const id   = (yield ssm.getParameter({ Name: '/viewcard/user_id',  WithDecryption: true }).promise() ).Parameter.Value;
+    const pass = (yield ssm.getParameter({ Name: '/viewcard/password', WithDecryption: true }).promise() ).Parameter.Value;
+    const ret = yield new ViewCardParser(id, pass)
+        .parse()
+        .catch(err => { throw new Error(err) });
+    console.log(JSON.stringify(ret))
+}).catch(err => { throw new Error(err) })
