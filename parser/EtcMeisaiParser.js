933453460d7401315fbe4a214eb9d28f512f5831
celeron1ghz <celeron1ghz@gmail.com>
2018-02-05 22:55:22 +0900

    apply class

--- parser/EtcMeisaiParser.js
+++ parser/EtcMeisaiParser.js
@@ -1,94 +1,61 @@
 'use strict';
 
-class EtcMeisaiParser {
+const PageParser = require('./PageParser');
+
+class EtcMeisaiParser extends PageParser {
     constructor(user_id, password) {
-        this.user_id  = user_id;
-        this.password = password;
+      super(user_id,password);
+      this.login_page_url = 'https://www2.etc-meisai.jp/etc/R?funccode=1013000000&nextfunc=1013000000';
     }
 
-    parse() {
-        const vo = require('vo');
-        const aws = require('aws-sdk');
-        const s3  = new aws.S3({ region: 'ap-northeast-1' });
-        const fs = require('fs');
-        //const nightmare = new require('nightmare')();
-        const nightmare = new require('nightmare')({ show: true });
-        const id   = this.user_id;
-        const pass = this.password;
+    login(nightmare) {
+      nightmare
+         // login
+        .type("input[name=risLoginId]", this.user_id)
+        .type("input[name=risPassword]", this.password)
+        .click("input[name=focusTarget]")
+        .wait("table.meisaiinfo")
+        // choose previous month
+        .click("table.meisaiinfo > tbody > tr:nth-child(3) > td > table:nth-child(3) > tbody > tr > td:nth-last-child(3) button")
+        .wait(1000)
+    }
 
-        return vo(function*(){
-            const url  = 'https://www2.etc-meisai.jp/etc/R?funccode=1013000000&nextfunc=1013000000';
+    parse_page(document) {
+      const css     = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
+      const rm_nbsp = (val)              => val.replace(/&nbsp;/g, "");
 
-            let result = [];
-            nightmare.viewport(1000, 1000)
-                .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-                .goto(url)
-                // login
-                .type("input[name=risLoginId]", id)
-                .type("input[name=risPassword]", pass)
-                .click("input[name=focusTarget]")
-                .wait("table.meisaiinfo")
-                // choose previous month
-                .click("table.meisaiinfo > tbody > tr:nth-child(3) > td > table:nth-child(3) > tbody > tr > td:nth-last-child(3) button")
-                .wait(1000)
-                
-            // for debugging
-            //yield nightmare.screenshot("/tmp/2.jpg")
-            //yield s3.putObject({ Bucket: 'billing-notifier', Key: '2.jpg', Body: fs.readFileSync('/tmp/2.jpg') }).promise();
-            
-            while (true)   {
-                const meisai = yield nightmare.evaluate(function () {
-                    const css     = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
-                    const rm_nbsp = (val)              => val.replace(/&nbsp;/g, "");
+      return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
+        const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
+        const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
+        const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
+        const from  = td1[0].split('<br>');
+        const to    = td1[1].split('<br>');
+        const price = td2.split('<br>');
+        const meta  = td4.split('<br>');
 
-                    try {
-                        return css(document, "tr.meisai, tr.meisai_r").map(function(tr){
-                            const td1 = css(tr, "td:nth-child(2) > table > tbody > tr > td > span").map(function(span){ return span.innerHTML });
-                            const td2 = tr.querySelector("td:nth-child(3) span").innerHTML;
-                            const td4 = tr.querySelector("td:nth-child(5) span").innerHTML;
-                            const from  = td1[0].split('<br>');
-                            const to    = td1[1].split('<br>');
-                            const price = td2.split('<br>');
-                            const meta  = td4.split('<br>');
+        return {
+          from_date:  rm_nbsp(from[0]),
+          from_time:  rm_nbsp(from[1]),
+          from_place: rm_nbsp(from[2]),
+          to_date:    to[0],
+          to_time:    to[1],
+          to_place:   to[2],
+          price:      price[2].replace(/,/g, ""),
+          type:       meta[0],
+          car_no:     meta[1],
+          etc_no:     meta[2],
+        }
+      });
+    }
 
-                            return {
-                                from_date:  rm_nbsp(from[0]),
-                                from_time:  rm_nbsp(from[1]),
-                                from_place: rm_nbsp(from[2]),
-                                to_date:    to[0],
-                                to_time:    to[1],
-                                to_place:   to[2],
-                                price:      price[2].replace(/,/g, ""),
-                                type:       meta[0],
-                                car_no:     meta[1],
-                                etc_no:     meta[2],
-                            }
-                        });
-                    } catch(e) {
-                        console.log("Error on parse:", e);
-                        return [];
-                    }
-                });
+    has_next_page(document) {
+      return document.querySelector("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
+    }
 
-                meisai.shift();
-                result = result.concat(meisai);
-
-                const next_button = yield nightmare.evaluate(
-                    () => document.querySelector("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
-                );
-
-                if (!next_button)  {
-                    break;
-                }
-
-                nightmare.click("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
-                    .wait("table.meisaiinfo")
-                    .wait(1000);
-            }
-
-            yield nightmare.end();
-            return result;
-        })
+    goto_next_page(nightmare) {
+      nightmare.click("table.meisaiinfo > tbody > tr:nth-child(4) table tbody tr td.plink:last-child button")
+        .wait("table.meisaiinfo")
+        .wait(1000);
     }
 }
 
--- parser/ViewCardParser.js
+++ parser/ViewCardParser.js
@@ -7,45 +7,45 @@ class ViewCardParser extends PageParser {
       super(user_id,password);
       this.login_page_url = 'https://viewsnet.jp/';
     }
-    
+
     login(nightmare) {
       nightmare
         .type("input[name=id]", this.user_id)
         .type("input[name=pass]", this.password)
         .click("input[type=image]")
         .wait(1000)
-        // 
+        //
         .click("a#vucGlobalNavi_LnkV0300_001Header")
         .wait(1000)
         //
         .click("a#LnkYotei")
         .wait(1000)
     }
-    
+
     parse_page(document) {
-        const css = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
-        const trs = css(document, "div#DivDetailInfo table tbody tr");
-        trs.shift();
+      const css = (parent, selector) => [].slice.apply(parent.querySelectorAll(selector));
+      const trs = css(document, "div#DivDetailInfo table tbody tr");
+      trs.shift();
 
-        return trs.map(function(tr){
-            const td1 = css(tr, "td:nth-child(1)");
-            const td2 = css(tr, "td:nth-child(2)");
-            const td3 = css(tr, "td:nth-child(3)");
-            const td4 = css(tr, "td:nth-child(4)");
+      return trs.map(function(tr){
+        const td1 = css(tr, "td:nth-child(1)");
+        const td2 = css(tr, "td:nth-child(2)");
+        const td3 = css(tr, "td:nth-child(3)");
+        const td4 = css(tr, "td:nth-child(4)");
 
-            return {
-                date:    (td1.length != 0 ? css(td1[0], "span").map(e => e.textContent).join("/") : ""),
-                card_no: (td2.length != 0 ? css(td2[0], "span")[0].innerHTML : ""),
-                shop:    (td3.length != 0 ? css(td3[0], "strong")[0].textContent : ""),
-                price:   (td4.length != 0 ? css(td4[0], "strong")[0].textContent.replace(/,/g, "") : ""),
-            };
-        });
+        return {
+          date:    (td1.length != 0 ? css(td1[0], "span").map(e => e.textContent).join("/") : ""),
+          card_no: (td2.length != 0 ? css(td2[0], "span")[0].innerHTML : ""),
+          shop:    (td3.length != 0 ? css(td3[0], "strong")[0].textContent : ""),
+          price:   (td4.length != 0 ? css(td4[0], "strong")[0].textContent.replace(/,/g, "") : ""),
+        };
+      });
     }
-    
+
     has_next_page(document) {
       return document.querySelector("#LnkNextBottom")
     }
-    
+
     goto_next_page(nightmare) {
       nightmare.click("#LnkNextBottom")
         .wait("div#DivDetailInfo")
